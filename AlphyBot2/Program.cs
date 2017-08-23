3aa202a55e26cd07d070b41b35731426ef0823e2
Alphuite <contact@alphuite.com>
2017-08-23 12:50:31 +0200

    added functions for IRC

--- AlphyBot2/Program.cs
+++ AlphyBot2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace AlphyBot2
 {
@@ -10,6 +11,29 @@ namespace AlphyBot2
     {
         static void Main(string[] args)
         {
+            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "alphybot", "password");
+            irc.joinRoom("alphuite");
+
+            Console.WriteLine("This is a test message xd");
+
+            string a;
+            string b;
+            while (true)
+            {
+                string message = irc.readMessage();
+                a = message;
+                if (a != b)
+                {
+                    Console.WriteLine(a);
+                    a = b;
+                }
+                
+
+                if (message.Contains("!test"))
+                {
+                    irc.sendChatMessage("tested!");
+                }
+            }
         }
     }
 }
