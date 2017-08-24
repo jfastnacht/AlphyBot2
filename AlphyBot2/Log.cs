451c147ff6efe6f40df6acf81795339cf02c8884
Alphuite <contact@alphuite.com>
2017-08-25 01:43:09 +0200

    Added log class and parsed IRC message into an object

---
+++ AlphyBot2/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphyBot2
+{
+    class Log
+    {
+        // Console logs a twitch chat message
+        static public void chatMessage(string timeStamp, string user, string channel, string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("[" + timeStamp + "]");
+            Console.ResetColor();
+            Console.WriteLine(" <" + user + "> " + channel + " : " + text);
+        }
+
+        // Logs a system message (e.g. response from the IRC server that isn't a chat message)
+        static public void systemMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
--- AlphyBot2/Program.cs
+++ AlphyBot2/Program.cs
@@ -3,38 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AlphyBot2
 {
     class Program
     {
+        public class Message
+        {
+            public string Text { get; set; }
+            public string Channel { get; set; }
+            public string UserName { get; set; }
+            public string TimeStamp { get; set; }
+            public Message(){}
+            public Message(string text, string channel, string userName, string timeStamp)
+            {
+                Text = text;
+                Channel = channel;
+                UserName = userName;
+                TimeStamp = timeStamp;
+            }
+        }
+
+        public static String GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
         static void Main(string[] args)
         {
+
             // Join a Twitch Channel
             IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "alphybot", "oauth:supersecretpasswordxd");
             irc.joinRoom("alphuite");
-            
+            irc.sendChatMessage("Hi! AlphyBot online since " + GetTimestamp(DateTime.Now));
+
             // Main Loop
             while(true)
             {
                 // Read last IRC message
                 string message = irc.readIrcMessage();
+                Message m = new Message();
 
-                // Echo **all** IRC messages
-                Console.WriteLine(message);
-
-                // Test Chat-Command
-                if (message.Contains("!test"))
+                // Parse the IRC message for twitch message
+                Regex regex = new Regex(@"\:\w+!\w+@\w+.tmi.twitch.tv PRIVMSG #\w+ :.+");
+                Match match = regex.Match(message);
+                if (match.Success)
                 {
-                    irc.sendChatMessage("it works mayhaps!");
+                    // Create Message
+                    m.UserName = match.Value.Split('!')[0].Replace(':', ' ').Trim();
+                    m.Text = Regex.Replace(match.Value, "\\:\\w+!\\w+@\\w+.tmi.twitch.tv PRIVMSG #\\w+ :", " ").Trim();
+                    m.Channel = Regex.Replace(match.Value, "\\:\\w+!\\w+@\\w+.tmi.twitch.tv PRIVMSG ", " ").Split(':')[0].Trim();
+                    m.TimeStamp = GetTimestamp(DateTime.Now);
+
+                    // Write Message
+                    Log.chatMessage(m.TimeStamp, m.UserName, m.Channel, m.Text);
+
+                    // Test Chat-Command
+                    if (m.Text.StartsWith("!test"))
+                    {
+                        irc.sendChatMessage("it works mayhaps!");
+                    }
+
+                } else
+                {
+                    // Write all IRC messages that aren't parsed as Twitch chat messages.
+                    Log.systemMessage(message);
                 }
 
                 // Reply with a Pong to the server, in case of Ping
                 if (message.Equals("PING :tmi.twitch.tv"))
                 {
                     irc.sendIrcMessage("PONG :tmi.twitch.tv");
-                    Console.WriteLine("PONG :tmi.twitch.tv"); // Just to make sure
+                    Log.systemMessage("PONG :tmi.twitch.tv"); // Just to make sure
                 }
+
             }
         }
     }
